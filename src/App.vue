27654b1bf9afc43c7ec215f3f18ee0862ee94601
Craig McCaskill <craigmccaskill@Craigs-MacBook-Pro.local>
2021-08-02 22:27:25 +0100

    Tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <my-navigation></my-navigation>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+import Navigation from './components/Navigation'
+  export default {
+     components:{
+         'my-navigation':Navigation
+     }
   }
-}
 </script>
 
 <style>
@@ -21,6 +18,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
