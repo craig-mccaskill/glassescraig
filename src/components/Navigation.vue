1e00ba1f33336fed368d956c5c6fabfb5a5d0236
Craig McCaskill <craigmccaskill@Craigs-MacBook-Pro.local>
2021-08-04 22:49:30 +0100

    Fix mobile dropdown mobile

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -38,7 +38,7 @@
               focus:outline-none
               focus:ring-2 focus:ring-inset focus:ring-indigo-500
             "
-            aria-expanded="false"
+            @click="isOpen = !isOpen"
           >
             <span class="sr-only">Open menu</span>
             <!-- Heroicon name: outline/menu -->
@@ -163,7 +163,7 @@
     </div>
 
     <!--Mobile menu, show/hide based on mobile menu state.-->
-  <div class="sm:hidden" id="mobile-menu">
+  <div class="sm:hidden" :class="isOpen ? 'block' : 'hidden'" id="mobile-menu">
     <div class="px-2 pt-2 pb-3 space-y-1">
       <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
       <a href="#" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Dashboard</a>
@@ -181,5 +181,10 @@
 <script>
 export default {
   name: "Navigation",
+  data() {
+    return {
+      isOpen: false,
+    }
+  },
 };
 </script>
